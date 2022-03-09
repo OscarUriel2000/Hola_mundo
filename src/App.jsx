913080b0e505bb766dbc8e703daf89b3d20f6838
Oscar Uriel Alcocer Delfin <isc18350287@gmail.com>
2022-03-09 16:12:47 -0600

    Primer Hola mundo

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Saludo from "./Saludo"
+
+const App = () =>{
+  return (
+    <div className="App">
+      <Saludo />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Saludo.jsx
@@ -0,0 +1,5 @@
+const Saludo =() => {
+    return <h1>Mi segundo Hola mundo </h1>;
+};
+
+export default Saludo;
